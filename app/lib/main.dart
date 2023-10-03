a20c16526a320bba5ed0f323ede332acd7f9cf9a
bsk dhanush <bskdhanush@gmail.com>
2023-10-02 20:41:09 -0500

    updated main.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter app after commit',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'update after commit app:',
             ),
             Text(
               '$_counter',
